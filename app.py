f52cd53612627616c2b7cfea5fd337c029184a61
blueonline07 <ldkhang1201@gmail.com>
2025-04-15 15:28:47 +0700

    ok

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
-from flask import Flask
-from controllers.data_controller import data_bp
+from flask import Flask, request
 from services.mqtt import AdafruitService
+
 from flask_cors import CORS
 from flask_socketio import SocketIO
 
 
 app = Flask(__name__)
+
 CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
-AdafruitService(socket=socketio)
-app.register_blueprint(data_bp, url_prefix='/data')
+
+sv = AdafruitService(socket=socketio)
+
+@app.route('/<feed>', methods=['POST'])
+def post_data(feed):
+    val = request.json.get('value')
+    sv.publish_val(feed, val)
+    return f"value {val} added to feed {feed}"
 
 if __name__ == '__main__':
     socketio.run(app)
