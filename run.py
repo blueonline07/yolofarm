9fb4d368a7ed77d89d74f1de57cebdb61da34ce1
LemonHV <matruongvu1@gmail.com>
2025-04-22 21:34:11 +0700

    deploy ci/cd

--- run.py
+++ run.py
@@ -2,5 +2,9 @@ from app import create_app
 
 socket, app = create_app()
 
+@app.route('/')
+def hello_world():
+    return 'IoT Backend API'
+
 if __name__ == '__main__':
     socket.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
